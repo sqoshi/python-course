b92d46bf5af8e2f3e41d2faa13e1164ccf924212
Piotr <piotrpopis98@icloud.com>
2020-03-03 14:06:31 +0100

    finished

--- List01/Task09.py
+++ List01/Task09.py
@@ -1,56 +1,28 @@
-from math import sin, tan, cos
+from cmath import sin
+import re
 
 
-def cal():
-    num1 = int(input("Number 1 --> "))
-    sign = input("Sign --> ")
-    num2 = 1
-    if sign != 'sin' and sign != 'cos' and sign != 'tan':
-        num2 = int(input("Last number --> "))
+def ret(s):
+    s = str(s)
 
-    if sign == "+" or sign == "-" or sign == "*" or sign == "/" or sign == "^":
-        print("You want to calculate %s%s%s." % (num1, sign, num2))
-    elif sign == 'sin' and sign == 'cos' and sign == 'tan':
-        print("You want to calculate %s(%s)." % (sign, num1))
-
-    if sign == "+":
-        result = int(num1) + int(num2)
-        print("The result is %s." % (result))
-
-    elif sign == "-":
-        result = int(num1) - int(num2)
-        print("The result is %s." % (result))
-
-    elif sign == "*":
-        result = num1 * num2
-        print("The result is %s." % (result))
-
-    elif sign == "/":
-        result = num1 / num2
-        print("The result is %s." % (result))
-
-    elif sign == "^":
-        result = num1 ** num2
-        print("The result is %s." % (result))
-
-    elif sign == "%":
-        result = num1 % num2
-        print("The result is %s." % (result))
-
-    elif sign == "sin":
-        result = sin(num1)
-        print("The result is %s." % (result))
-
-    elif sign == "cos":
-        result = cos(num1)
-        print("The result is %s." % (result))
-
-    elif sign == "tan":
-        result = tan(num1)
-        print("The result is %s." % (result))
-
-    else:
-        print("Something went wrong.")
+    if s.isdigit():
+        return float(s)
+    for c in ('-', '+', '*', '/', '^'):
+        left, op, right = s.partition(c)
+        if op == '*':
+            return ret(left) * ret(right)
+        elif op == '/':
+            return ret(left) / ret(right)
+        elif op == '+':
+            return ret(left) + ret(right)
+        elif op == '-':
+            return ret(left) - ret(right)
+        elif op == '^':
+            return ret(left) ** ret(right)
 
 
-cal()
+def func(x):
+    return sin(x)
+
+
+print(ret('4*3+2*6^2'))
