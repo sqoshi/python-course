b4baddd5ee81af1c838dd43b841c0eb59d38b864
Piotr <piotrpopis98@icloud.com>
2020-03-09 21:33:14 +0100

    task03 fnished

---
+++ List02/task_03/task_03.py
@@ -0,0 +1,10 @@
+import os
+
+# ls -p | grep -v / | rename 'y/A-Z/a-z/'
+
+path = "/home/piotr/Documents/python-course/List02/task_03"
+with os.scandir(path) as it:
+    for entry in it:
+        if not entry.name.startswith('.'):
+            print(entry.name)
+            os.replace(entry.name, entry.name.lower())
